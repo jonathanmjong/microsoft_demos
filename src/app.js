10ccaf911caef9fa5033662ec7c912cc2c14f64a
Jonathan Jong <mjong2@gmail.com>
2020-06-15 00:34:05 -0500

    added port

--- src/app.js
+++ src/app.js
@@ -3,10 +3,11 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
+const port = process.env.PORT || 5901
 
 publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server running on port 3000')
 })
